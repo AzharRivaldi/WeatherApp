3163cb51705924e43989ad6ced807e53c96f7166
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2020-02-26 17:29:54 +0700

    Update ApiWeather.java

--- Weather/app/src/main/java/com/azhar/weather/api/ApiWeather.java
+++ Weather/app/src/main/java/com/azhar/weather/api/ApiWeather.java
@@ -6,6 +6,6 @@ package com.azhar.weather.api;
 
 public class ApiWeather {
 
-    public static String API_URL = "http://api.openweathermap.org/data/2.5/forecast/?lat=-{lat}&lon={long}&units=metric&APPID={apikeyZ}";
+    public static String API_URL = "http://api.openweathermap.org/data/2.5/forecast/?lat=-{lat}&lon={long}&units=metric&APPID={apikey}";
 
 }
